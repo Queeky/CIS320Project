003cbcb8052b53ffd3cacd5ad3fa12fd57889ff3
Shyla-Judy <129998969+Shyla-Judy@users.noreply.github.com>
2023-10-23 16:14:23 -0400

    Add files via upload

--- about.php
+++ about.php
@@ -4,9 +4,22 @@
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
-        <title>About</title>
+        <title>About Us</title>
     </head>
     <body>
         <?php include("includes/header.inc.php"); ?>
+        <div> </div>
+        <h1> History of the Shelter </h1>
+        <p> This shelter was created in 2000. The capacity of the shelter is 20 animals. This is a no kill shelter. </p>
+
+
+        
+        <h2> How to Adopt </h2> 
+        <p> When you come to our shelter the workers wiil help you find a pet that fits with your lifestyle.</P>
+        <p> The dogs and cats at the shelt are dewormed, microchipped, vaccinated, and spayed or neutered.</P>
+        <p> In order to adpot a pet you have to submit an application, and you will have to answer some questions just to make sure you are a good candidate for adopting a pet. </P>
+
+        <h3> The Shelter Hours </h3>
+        <p> The Shelter is open Tuesday through Friday from 9am to 9pm </p>
     </body>
 </html>
